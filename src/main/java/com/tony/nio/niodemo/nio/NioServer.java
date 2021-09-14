cc0e5ea5e88c4750e2b1477a0a0e8904afecb51c
gaoweidong <511872270@qq.com>
2021-09-14 19:44:35 +0800

    add

--- src/main/java/com/tony/nio/niodemo/nio/NioServer.java
+++ src/main/java/com/tony/nio/niodemo/nio/NioServer.java
@@ -16,28 +16,22 @@ import java.util.LinkedList;
  */
 public class NioServer {
 
-    // what why how
     public static void main(String[] args) throws Exception {
         LinkedList<SocketChannel> clients = new LinkedList<>();
-        //服务端开启监听：接受客户端
         ServerSocketChannel ss = ServerSocketChannel.open();
         ss.bind(new InetSocketAddress(9090));
-        // 重点 OS NONBLOCKING
+        // 重点 NONBLOCKING
         // 接收客户端不阻塞
         ss.configureBlocking(false);
-//        ss.setOption(StandardSocketOptions.TCP_NODELAY, false);
         while (true) {
             // 接受客户端的连接
             Thread.sleep(1000);
-            // 不会阻塞？-1 NULL
+            // 立马返回
             SocketChannel client = ss.accept();
-            //accept  调用内核了：
-            //1，没有客户端连接进来，返回值？在BIO 的时候一直卡着，但是在NIO，不卡着，返回-1，NULL
-            // 如果来客户端的连接，accept返回的是这个客户端的fd
             if (client == null) {
-             //   System.out.println("没有客户端连接");
+                Thread.sleep(3000);
+                System.out.println("no client connect");
             } else {
-                //重点socket（服务端的listen socket<连接请求三次握手后，往我这里扔，我去通过accept 得到  连接的socket>，连接socket<连接后的数据读写使用的> ）
                 client.configureBlocking(false);
                 int port = client.socket().getPort();
                 System.out.println("client..port: " + port);
@@ -47,7 +41,7 @@ public class NioServer {
             ByteBuffer buffer = ByteBuffer.allocateDirect(4096);
             // 遍历已经链接进来的客户端能不能读写数据
             for (SocketChannel c : clients) {
-                // >0  -1  0  不会阻塞
+                // 返回值: >0 -1 0,不会阻塞
                 int num = c.read(buffer);
                 if (num > 0) {
                     buffer.flip();
